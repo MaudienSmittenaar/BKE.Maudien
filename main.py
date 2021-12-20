3aa44069ebf19a0f9449c062f2906e98a352bcea
Maudien Smittenaar <57255283+MaudienSmittenaar@users.noreply.github.com>
2021-12-20 13:49:39 +0000

    De computer zet nu de winnende zet

--- main.py
+++ main.py
@@ -1,23 +1,39 @@
-from _ml import MLAgent,  load, validate, RandomAgent, plot_validation
-from _core import is_winner, opponent
- 
- 
-class MyAgent(MLAgent):
-    def evaluate(self, board):
-        if is_winner(board, self.symbol):
-            reward = 1
-        elif is_winner(board, opponent[self.symbol]):
-            reward = -1
-        else:
-            reward = 0
-        return reward
-    
- 
-my_agent = load('MyAgent_3000')
-my_agent.learning = False
- 
-validation_agent = RandomAgent()
- 
-validation_result = validate(agent_x=my_agent, agent_o=validation_agent, iterations=100)
- 
-plot_validation(validation_result)
+import random
+from bke import EvaluationAgent, start
+
+#class MyRandomAgent(EvaluationAgent):
+ # def evaluate(self, board, my_symbol, opponent_symbol):
+  #  return random.randint(1, 500)
+  
+class MyCapableAgent(EvaluationAgent):
+  def getBoardCopy(board):
+    boardCopy = []
+    for i in board:
+       boardCopy.append(i)
+     return boardCopy
+
+   def isWinner(bo, le):
+     return ((bo[7] == le and bo[8] == le and bo[9] == le) or
+     (bo[4] == le and bo[5] == le and bo[6] == le) or
+     (bo[1] == le and bo[2] == le and bo[3] == le) or
+     (bo[7] == le and bo[4] == le and bo[1] == le) or
+     (bo[8] == le and bo[5] == le and bo[2] == le) or
+     (bo[9] == le and bo[6] == le and bo[3] == le) or
+     (bo[7] == le and bo[5] == le and bo[3] == le) or
+     (bo[9] == le and bo[5] == le and bo[1] == le))
+
+   def evaluate(self, board, my_symbol, opponent_symbol):
+     for i in range (0,9):
+       boardCopy = MyCapableAgent.getBoardCopy(board)
+       if boardCopy[i] != None:
+         boardCopy[i] = my_symbol
+         if MyCapableAgent.isWinner(boardCopy, my_symbol):
+           return i
+
+     return random.randint(0,8)
+
+
+
+ #my_random_agent = MyRandomAgent()
+ my_agent = MyCapableAgent()
+start(player_o = my_agent)
