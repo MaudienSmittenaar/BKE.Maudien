26f0f75c4ae4970ca4ebeae9c7adb6cee4099529
Maudien Smittenaar <57255283+MaudienSmittenaar@users.noreply.github.com>
2021-12-22 09:23:27 +0000

    Kleine foute posities opgelost

--- main.py
+++ main.py
@@ -25,43 +25,42 @@ itterate = 150
 training = 500000
 play = "y"
 while play == "y":
-   print("\n 1: Speel met 2 spelers \n 2: Speel tegen een getrainde computer \n 3: Train een AI \n 4: Controleer hoe goed een bepaalde agent is \n 5: Test hoe goed bepaalde hyperparameter combinaties werken \n Kies wat u wilt spelen: \n")
-   choice = input()
+  print("\n 1: Speel met 2 spelers \n 2: Speel tegen een getrainde computer \n 3: Train een AI \n 4: Controleer hoe goed een bepaalde agent is \n 5: Test hoe goed bepaalde hyperparameter combinaties werken \n Kies wat u wilt spelen: \n")
+  choice = input()
 
 
 
-if choice == '1':
-  start()
+  if choice == '1':
+    start()
 
-if choice == '2':
-  print("Tegen welke agent wilt u spelen? Wenst u tegen de agent van dit programma te willen spelen, vul dan 'agent1' in")
-  play_agent = input()
+  if choice == '2':
+    print("Tegen welke agent wilt u spelen? Wenst u tegen de agent van dit programma te willen spelen, vul dan 'agent1' in")
+    play_agent = input()
 
-  my_agent = load(play_agent)
-  my_agent.learning = False
-  start(player_x=my_agent)    
+    my_agent = load(play_agent)
+    my_agent.learning = False
+    start(player_x=my_agent)    
 
-if choice == '3':
-  print("Hoe wil je je agent noemen?")
-  name = input()
-  while training >= 500000:
-    print("Hoe vaak moet uw agent getraind worden? Maximaal 500000")
-    training = int(input())
+  if choice == '3':
+    print("Hoe wil je je agent noemen?")
+    name = input()
+    while training >= 500000:
+      print("Hoe vaak moet uw agent getraind worden? Maximaal 500000")
+      training = int(input())
 
-  print("Wilt u de hyperparameters aanpassen? (y/n)")
-  if input() == "y":
-    print("Uitleg voor hyperparameters tussen 1 en 0")
-    chosen_alpha = float(input())
-    chosen_epsilon = float(input())
-    my_agent = 
-MyAgent(alpha=alpha_chosen, epsilon=chosen_epsilon)
-  else: 
-    my_agent = MyAgent()
+    print("Wilt u de hyperparameters aanpassen? (y/n)")
+    if input() == "y":
+      print("Uitleg voor hyperparameters tussen 1 en 0")
+      chosen_alpha = float(input())
+      chosen_epsilon = float(input())
+      my_agent = MyAgent(alpha=chosen_alpha, epsilon=chosen_epsilon)
+    else: 
+      my_agent = MyAgent()
 
-  train(my_agent, training)
-  save(my_agent, name)
+    train(my_agent, training)
+    save(my_agent, name)
 
-  print("\nJe agent is getraind en opgeslagen onder: " + name)
+    print("\nJe agent is getraind en opgeslagen onder: " + name)
 
   if choice == '4':
     print("Als de grafiek getekend is, klik het weg om verder te gaan")
@@ -70,44 +69,44 @@ MyAgent(alpha=alpha_chosen, epsilon=chosen_epsilon)
     print("Wil je dat uw agent X of O is? Voor uw informatie: X begint altijd")
     symbol = input()
 
-   my_agent = load(name)
-   my_agent.learning = False
+    my_agent = load(name)
+    my_agent.learning = False
 
-   validation_agent = RandomAgent()
+    validation_agent = RandomAgent()
 
-if symbol == "x" or symbol == "X":
-     validation_result = validate(agent_x=my_agent, agent_o=validation_agent, iterations=100)
+    if symbol == "x" or symbol == "X":
+      validation_result = validate(agent_x=my_agent, agent_o=validation_agent, iterations=100)
 
-if symbol == "o" or symbol == "O":
-     validation_result = validate(agent_o=my_agent, agent_x=validation_agent, iterations=100)
+    if symbol == "o" or symbol == "O":
+      validation_result = validate(agent_o=my_agent, agent_x=validation_agent, iterations=100)
 
-plot_validation(validation_result)
+    plot_validation(validation_result)
 
 
-if choice == "5":
-  print("Als de grafiek getekend is, klik het weg om verder te gaan")
+  if choice == "5":
+    print("Als de grafiek getekend is, klik het weg om verder te gaan")
 
-   random.seed(1)
+    random.seed(1)
 
-   print("Uitleg voor hyperparameters tussen 1 en 0")
-   print("alpha:")
-   chosen_alpha = float(input())
-   print("epsilon:")
-   chosen_epsilon = float(input())
+    print("Uitleg voor hyperparameters tussen 1 en 0")
+    print("alpha:")
+    chosen_alpha = float(input())
+    print("epsilon:")
+    chosen_epsilon = float(input())
 
-   while itterate >= 51:
-     print("Hoeveel iteraties wil je doen? (max 50)")
-     itterate = int(input())
+    while itterate >= 51:
+      print("Hoeveel iteraties wil je doen? (max 50)")
+      itterate = int(input())
 
-   my_agent = MyAgent(alpha=chosen_alpha, epsilon=chosen_epsilon)
-   random_agent = RandomAgent()
+  my_agent = MyAgent(alpha=chosen_alpha, epsilon=chosen_epsilon)
+  random_agent = RandomAgent()
 
-   train_and_plot(
+  train_and_plot(
        agent=my_agent,
     validation_agent=random_agent,
        iterations=30,
        trainings=itterate,
        validations=1000)
 
-print("Wilt u doorgaan? (y/n)")
-play = input()
+  print("Wilt u doorgaan? (y/n)")
+  play = input()
